0070c5554aadf0b0db7d9809c3198ac099cc91c4
Yasuhiro Usutani <y_usutani@mac.com>
2022-04-02 08:57:10 +0900

    1

---
+++ db/migrate/20220401235502_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :posts do |t|
+      t.text :body
+      t.belongs_to :person, null: false, foreign_key: true
+      t.boolean :published
+
+      t.timestamps
+    end
+  end
+end
